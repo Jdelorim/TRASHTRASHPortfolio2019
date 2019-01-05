12359b7f43861766dbe2953b917531df1d75a5d4
Joshua deLorimier <TRASHTRASH_HQ@Joshuas-Mac-Pro.localdomain>
2019-01-05 14:33:06 -0500

    fixed link

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -5,20 +5,20 @@ document.addEventListener("DOMContentLoaded", () => {
     var tooltiper = document.querySelectorAll('.tooltipped');
     M.Tooltip.init(tooltiper, {});
 
-    console.log("firstTime:", firstTime);
+    //console.log("firstTime:", firstTime);
     startTrash();
 
     function startTrash() {
-        console.log("in start");
+        //console.log("in start");
         firstTime = false;
-        console.log("firstTime:", firstTime);
+        //console.log("firstTime:", firstTime);
         setTimeout(trashBegin, 10000);
 
     }
 
     function trashBegin() {
-        console.log("in begin");
-        console.log("firstTime:", firstTime);
+        //console.log("in begin");
+        //console.log("firstTime:", firstTime);
         myInt = setInterval(myTimer, 1000 / 8);
         myTime = setTimeout(trashStop, 10000);
     }
@@ -30,20 +30,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
         (logo.style.color == "black") ? logo.style.color = "white": logo.style.color = "black";
         (bck.style.backgroundColor == "white") ? bck.style.backgroundColor = "black": bck.style.backgroundColor = "white";
-        console.log('in strobe');
+        
+        //console.log('in strobe');
+        
         if (firstTime === true) {
             clearInterval(myInt);
             clearTimeout(myTime);
-           
-           
             trashStop();
         }
     }
 
     function trashStop() {
-        console.log("hit stop");
+        //console.log("hit stop");
+        
         firstTime = true;
-        console.log("firstTime", firstTime);
+        
+        //console.log("firstTime", firstTime);
+        
         var logo = document.getElementById("trashtrash");
         var bck = document.getElementById("splashTrash");
         logo.style.color = "white";
